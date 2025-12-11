b8f5b3aadde1235157f3dd15bab440e802c25ad3
GeorgII <george.webfullstack@gmail.com>
2025-12-11 19:58:45 +0100

    Add `Array.php` usage example and update Composer script to include it

---
+++ src/Usage/Primitive/Array.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PhpTypedValues\Usage\Primitive;
+
+require_once 'vendor/autoload.php';
+
+use const PHP_EOL;
+
+use PhpTypedValues\Exception\ArrayTypeException;
+
+/**
+ * Array.
+ */
+echo PHP_EOL . '> ARRAY' . PHP_EOL;
+
+try {
+    throw new ArrayTypeException('Array type exception occurred');
+} catch (ArrayTypeException) {
+    // suppress
+}
