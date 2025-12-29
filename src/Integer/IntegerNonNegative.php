73bec688768110d9ad7795f120ec4bac5de1fa9a
GeorgII <george.webfullstack@gmail.com>
2025-12-29 23:06:32 +0100

    Add `tryFromInt`, `tryFromString`, and `tryFromMixed` methods to integer types
    - Implemented new type conversion methods in `IntegerPositive`, `IntegerNonNegative`, `IntegerStandard`, `IntegerWeekDay`, and `MariaDb\IntegerTiny`.
    - Refactored `IntType` to use abstract method definitions for better extensibility.

--- src/Integer/IntegerNonNegative.php
+++ src/Integer/IntegerNonNegative.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace PhpTypedValues\Integer;
 
+use Exception;
 use PhpTypedValues\Base\Primitive\Integer\IntType;
+use PhpTypedValues\Base\Primitive\PrimitiveType;
 use PhpTypedValues\Exception\IntegerTypeException;
+use PhpTypedValues\Exception\TypeException;
+use PhpTypedValues\Undefined\Alias\Undefined;
+use Stringable;
 
+use function is_bool;
+use function is_int;
+use function is_string;
 use function sprintf;
 
 /**
@@ -64,6 +72,74 @@ readonly class IntegerNonNegative extends IntType
     public function value(): int
     {
         return $this->value;
+    }
+
+    /**
+     * @template T of PrimitiveType
+     *
+     * @param T $default
+     *
+     * @return static|T
+     *
+     * @psalm-return (static&IntType)|T
+     */
+    public static function tryFromInt(
+        int $value,
+        PrimitiveType $default = new Undefined(),
+    ): static|PrimitiveType {
+        try {
+            /** @var static&IntType */
+            return static::fromInt($value);
+        } catch (TypeException) {
+            /* @var T $default */
+            return $default;
+        }
+    }
+
+    /**
+     * @template T of PrimitiveType
+     *
+     * @param T $default
+     *
+     * @return static|T
+     */
+    public static function tryFromString(
+        string $value,
+        PrimitiveType $default = new Undefined(),
+    ): static|PrimitiveType {
+        try {
+            /** @var static */
+            return static::fromString($value);
+        } catch (Exception) {
+            /** @var T */
+            return $default;
+        }
+    }
+
+    /**
+     * @template T of PrimitiveType
+     *
+     * @param T $default
+     *
+     * @return static|T
+     */
+    public static function tryFromMixed(
+        mixed $value,
+        PrimitiveType $default = new Undefined(),
+    ): static|PrimitiveType {
+        try {
+            /** @var static */
+            return match (true) {
+                is_int($value) => static::fromInt($value),
+                //                $value instanceof self => static::fromInt($value->value()),
+                is_bool($value) => static::fromInt($value ? 1 : 0),
+                is_string($value) || $value instanceof Stringable => static::fromString((string) $value),
+                default => throw new TypeException('Value cannot be cast to int'),
+            };
+        } catch (Exception) {
+            /** @var T */
+            return $default;
+        }
     }
 
     /**
