46e3b3653c45681a48c43105fad90afb7ffdc023
GeorgII <georgiiweb@gmail.com>
2026-01-05 23:14:18 +0100

    Extend string type to support casting from and to float, int, and bool. Improve type safety and add `tryFrom` methods for better error handling.

--- src/String/StringNonBlank.php
+++ src/String/StringNonBlank.php
@@ -7,12 +7,17 @@ namespace PhpTypedValues\String;
 use Exception;
 use PhpTypedValues\Base\Primitive\PrimitiveTypeAbstract;
 use PhpTypedValues\Base\Primitive\String\StringTypeAbstract;
+use PhpTypedValues\Exception\Bool\BoolTypeException;
+use PhpTypedValues\Exception\Float\FloatTypeException;
+use PhpTypedValues\Exception\Integer\IntegerTypeException;
 use PhpTypedValues\Exception\String\StringTypeException;
 use PhpTypedValues\Exception\TypeException;
 use PhpTypedValues\Undefined\Alias\Undefined;
 use Stringable;
 
-use function is_scalar;
+use function is_bool;
+use function is_float;
+use function is_int;
 use function is_string;
 use function sprintf;
 use function trim;
@@ -44,7 +49,6 @@ readonly class StringNonBlank extends StringTypeAbstract
             throw new StringTypeException(sprintf('Expected non-blank string, got "%s"', $value));
         }
 
-        /** @var non-empty-string $value */
         $this->value = $value;
     }
 
@@ -56,44 +60,30 @@ readonly class StringNonBlank extends StringTypeAbstract
         return new static($value);
     }
 
-    /** @return non-empty-string */
-    public function value(): string
+    /**
+     * @throws FloatTypeException
+     * @throws StringTypeException
+     */
+    public static function fromFloat(float $value): static
     {
-        return $this->value;
-    }
-
-    public function jsonSerialize(): string
-    {
-        return $this->toString();
-    }
-
-    public function isTypeOf(string ...$classNames): bool
-    {
-        foreach ($classNames as $className) {
-            if ($this instanceof $className) {
-                return true;
-            }
-        }
-
-        return false;
+        return new static(static::floatToString($value));
     }
 
     /**
-     * @return non-empty-string
+     * @throws FloatTypeException
+     * @throws StringTypeException
      */
-    public function toString(): string
+    public static function fromInt(int $value): static
     {
-        return $this->value();
+        return new static(static::intToString($value));
     }
 
-    public function isEmpty(): bool
+    /**
+     * @throws StringTypeException
+     */
+    public static function fromBool(bool $value): static
     {
-        return false;
-    }
-
-    public function isUndefined(): bool
-    {
-        return false;
+        return new static(static::boolToString($value));
     }
 
     /**
@@ -111,9 +101,73 @@ readonly class StringNonBlank extends StringTypeAbstract
             /** @var static */
             return match (true) {
                 is_string($value) => static::fromString($value),
-                $value instanceof Stringable, is_scalar($value) => static::fromString((string) $value),
+                is_float($value) => static::fromFloat($value),
+                is_int($value) => static::fromInt($value),
+                ($value instanceof self) => static::fromString($value->value()),
+                is_bool($value) => static::fromBool($value),
+                $value instanceof Stringable => static::fromString((string) $value),
                 default => throw new TypeException('Value cannot be cast to string'),
             };
+        } catch (Exception) {
+            /** @var T */
+            return $default;
+        }
+    }
+
+    /**
+     * @template T of PrimitiveTypeAbstract
+     *
+     * @param T $default
+     *
+     * @return static|T
+     */
+    public static function tryFromFloat(
+        float $value,
+        PrimitiveTypeAbstract $default = new Undefined(),
+    ): static|PrimitiveTypeAbstract {
+        try {
+            /** @var static */
+            return static::fromFloat($value);
+        } catch (Exception) {
+            /** @var T */
+            return $default;
+        }
+    }
+
+    /**
+     * @template T of PrimitiveTypeAbstract
+     *
+     * @param T $default
+     *
+     * @return static|T
+     */
+    public static function tryFromInt(
+        int $value,
+        PrimitiveTypeAbstract $default = new Undefined(),
+    ): static|PrimitiveTypeAbstract {
+        try {
+            /** @var static */
+            return static::fromInt($value);
+        } catch (Exception) {
+            /** @var T */
+            return $default;
+        }
+    }
+
+    /**
+     * @template T of PrimitiveTypeAbstract
+     *
+     * @param T $default
+     *
+     * @return static|T
+     */
+    public static function tryFromBool(
+        bool $value,
+        PrimitiveTypeAbstract $default = new Undefined(),
+    ): static|PrimitiveTypeAbstract {
+        try {
+            /** @var static */
+            return static::fromBool($value);
         } catch (Exception) {
             /** @var T */
             return $default;
@@ -138,5 +192,69 @@ readonly class StringNonBlank extends StringTypeAbstract
             /** @var T */
             return $default;
         }
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function toString(): string
+    {
+        return $this->value();
+    }
+
+    /**
+     * @throws FloatTypeException
+     */
+    public function toFloat(): float
+    {
+        return static::stringToFloat($this->value());
+    }
+
+    /**
+     * @throws IntegerTypeException
+     */
+    public function toInt(): int
+    {
+        return static::stringToInt($this->value());
+    }
+
+    /**
+     * @throws BoolTypeException
+     */
+    public function toBool(): bool
+    {
+        return static::stringToBool($this->value());
+    }
+
+    /** @return non-empty-string */
+    public function value(): string
+    {
+        return $this->value;
+    }
+
+    public function isEmpty(): bool
+    {
+        return false;
+    }
+
+    public function isUndefined(): bool
+    {
+        return false;
+    }
+
+    public function jsonSerialize(): string
+    {
+        return $this->toString();
+    }
+
+    public function isTypeOf(string ...$classNames): bool
+    {
+        foreach ($classNames as $className) {
+            if ($this instanceof $className) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
