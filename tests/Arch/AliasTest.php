06882a1841829e7b02216e7e2947aee60d9cebee
GeorgII <georgiiweb@gmail.com>
2026-01-25 16:21:35 +0100

    Refactor `AliasTest` for improved structure and readability
    Reorganized tests into a descriptive block for better maintainability and consistency across test cases.

--- tests/Arch/AliasTest.php
+++ tests/Arch/AliasTest.php
@@ -10,43 +10,45 @@ use PhpTypedValues\Base\Primitive\PrimitiveTypeInterface;
 use PhpTypedValues\Base\Primitive\String\StringTypeInterface;
 use PhpTypedValues\Base\Primitive\Undefined\UndefinedTypeInterface;
 
-arch('Alias Boolean')
-    ->expect('PhpTypedValues\Bool\Alias')
-    ->toBeClasses()
-    ->toBeFinal()
-    ->toExtend(BoolTypeAbstract::class)
-    ->toImplement(PrimitiveTypeInterface::class)
-    ->toImplement(BoolTypeInterface::class)
-    ->toBeReadonly();
+describe('Aliases', function () {
+    arch('Alias Boolean')
+        ->expect('PhpTypedValues\Bool\Alias')
+        ->toBeClasses()
+        ->toBeFinal()
+        ->toExtend(BoolTypeAbstract::class)
+        ->toImplement(PrimitiveTypeInterface::class)
+        ->toImplement(BoolTypeInterface::class)
+        ->toBeReadonly();
 
-arch('Alias Float')
-    ->expect('PhpTypedValues\Float\Alias')
-    ->toBeClasses()
-    ->toBeFinal()
-    ->toImplement(PrimitiveTypeInterface::class)
-    ->toImplement(FloatTypeInterface::class)
-    ->toBeReadonly();
+    arch('Alias Float')
+        ->expect('PhpTypedValues\Float\Alias')
+        ->toBeClasses()
+        ->toBeFinal()
+        ->toImplement(PrimitiveTypeInterface::class)
+        ->toImplement(FloatTypeInterface::class)
+        ->toBeReadonly();
 
-arch('Alias Integer')
-    ->expect('PhpTypedValues\Integer\Alias')
-    ->toBeClasses()
-    ->toBeFinal()
-    ->toImplement(PrimitiveTypeInterface::class)
-    ->toImplement(IntegerTypeInterface::class)
-    ->toBeReadonly();
+    arch('Alias Integer')
+        ->expect('PhpTypedValues\Integer\Alias')
+        ->toBeClasses()
+        ->toBeFinal()
+        ->toImplement(PrimitiveTypeInterface::class)
+        ->toImplement(IntegerTypeInterface::class)
+        ->toBeReadonly();
 
-arch('Alias String')
-    ->expect('PhpTypedValues\String\Alias')
-    ->toBeClasses()
-    ->toBeFinal()
-    ->toImplement(PrimitiveTypeInterface::class)
-    ->toImplement(StringTypeInterface::class)
-    ->toBeReadonly();
+    arch('Alias String')
+        ->expect('PhpTypedValues\String\Alias')
+        ->toBeClasses()
+        ->toBeFinal()
+        ->toImplement(PrimitiveTypeInterface::class)
+        ->toImplement(StringTypeInterface::class)
+        ->toBeReadonly();
 
-arch('Alias Undefined')
-    ->expect('PhpTypedValues\Undefined\Alias')
-    ->toBeClasses()
-    ->toBeFinal()
-    ->toImplement(PrimitiveTypeInterface::class)
-    ->toImplement(UndefinedTypeInterface::class)
-    ->toBeReadonly();
+    arch('Alias Undefined')
+        ->expect('PhpTypedValues\Undefined\Alias')
+        ->toBeClasses()
+        ->toBeFinal()
+        ->toImplement(PrimitiveTypeInterface::class)
+        ->toImplement(UndefinedTypeInterface::class)
+        ->toBeReadonly();
+});
